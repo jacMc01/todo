d7197b04538c10a8d6e8b9fdd4d05ff4f48865b7
Jacqueline Mckay H <jacquelinemc@outlook.es>
2024-04-01 17:34:05 -0600

    finished proyect

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'mackymckay.github.io/todo'
+  base: 'https://jacmc01.github.io/todo'
 })
